5fb34f1a053d4043e2a3677d852acc7781875d84
DK_Fox <golovin.anton92@gmail.com>
2018-03-22 00:03:08 +0200

    Start

--- src/components/Scheduler/index.js
+++ src/components/Scheduler/index.js
@@ -1,1 +1,68 @@
+//Core
+import React, { Component } from 'react';
 
+//Style
+import Style from './styles.scss';
+
+//Instrumental
+import Task from '../Task/';
+
+export default class Scheduler extends Component {
+    state = {
+        message: '',
+    };
+
+    _onSchedulerType = ({ target: { value }}) => {
+        if (value.length <= 10) {
+            this.setState({
+                message: value,
+            });
+        }
+    };
+
+    _sendScheduler = (e) => {
+        e.preventDefault();
+        const { message } = this.state;
+
+        if (message) {
+            this.setState({
+                message: '',
+            });
+        }
+    };
+
+
+    render () {
+        const { message } = this.state;
+
+        return (
+            <div className = { Style.scheduler }>
+                <main>
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input placeholder = 'Поиск...' type = 'text' />
+                    </header>
+                    <section>
+                        <form onSubmit = { this._sendScheduler }>
+                            <input placeholder = 'Описание моей новой задачи' type = 'text' onChange = { this._onSchedulerType } value = { message } />
+                            <button>Добавить задачу</button>
+                        </form>
+                        <ul>
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                            <Task />
+                        </ul>
+                    </section>
+                    <footer>
+                        <span>
+                            +
+                        </span>
+                        <code>code</code>
+                    </footer>
+                </main>
+            </div>
+        );
+    }
+}
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,1 +1,26 @@
+//Core
+import React, { Component } from 'react';
 
+//Style
+import Style from './styles.scss';
+
+
+export default class Task extends Component {
+    render () {
+        return (
+            <li className = { Style.task }>
+                <div>
+                    <span>
+                        +
+                    </span>
+                    <input type = ' text' value = 'Успешно пройти React-интенсив компании Lectrum' />
+                </div>
+                <div>
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                </div>
+            </li>
+        );
+    }
+}
